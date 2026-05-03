f45a9083cb4f4a149dcb4c38bf66092d1e629fea
sandrosouza1127-cyber <sandrosouza1127@gmail.com>
2026-05-03 18:54:04 -0300

    Update setup_tpm.sh

--- setup_tpm.sh
+++ setup_tpm.sh
@@ -20,7 +20,7 @@ tpm2_evictcontrol -C o -c 0x81010001 2>/dev/null
 HEX_SEED=$(openssl rand -hex 16)
 echo $HEX_SEED
 
-tpm2_createprimary -p "$HEX_SEED" -C o -g sha256 -G rsa -c primary.ctx
+tpm2_createprimary -p "$HEX_SEED" -C o -g sha1 -G rsa -c primary.ctx
 tpm2_readpublic -c primary.ctx -f pem -o endorsement_pub.pem
 tpm2_evictcontrol -C o -c primary.ctx 0x81010001
 
