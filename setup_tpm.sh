85b5f4191a82babcf43c5fc3a4e511a7bd161a66
sandrosouza1127-cyber <sandrosouza1127@gmail.com>
2026-05-03 19:10:07 -0300

    TPM.sh

--- setup_tpm.sh
+++ setup_tpm.sh
@@ -6,22 +6,17 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-echo "Atualizando..."
 apt update && apt upgrade -y
 
-echo "Instalando..."
-apt install -y tpm2-tools openssl
+apt install tpm2-tools openssl -y
 
-echo "Limpando TPM..."
 tpm2_clear
 
-tpm2_evictcontrol -C o -c 0x81010001 2>/dev/null
+tpm2_evictcontrol -C o -c 0x81010001
 
 HEX_SEED=$(openssl rand -hex 16)
 echo $HEX_SEED
 
-tpm2_createprimary -p "$HEX_SEED" -C o -g sha1 -G rsa -c primary.ctx
+tpm2_createprimary -p "$HEX_SEED" -C o -g sha256 -G rsa -c primary.ctx
 tpm2_readpublic -c primary.ctx -f pem -o endorsement_pub.pem
 tpm2_evictcontrol -C o -c primary.ctx 0x81010001
-
-echo "Finalizado"
